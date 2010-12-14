6c38940307afda877d9f01199127f010561e4589
Alex Hayton <alex.hayton@gmail.com>
2010-12-14 22:10:58 +0000

    Checked in some inital files. They won't work yet - need some tidying up

---
+++ lua/Experience.lua
@@ -0,0 +1,21 @@
+//
+// lua\GUIScoreboard.lua
+//
+// Created by: Alex Hayton (alex.hayton@gmail.com)
+//
+// Manages the experience bar
+//
+
+gMaxExperience = 1000
+
+function Experience_GetNumSkillsAvailable(experience)
+
+end
+
+function Experience_GetRank(experience)
+
+end
+
+function Experience_GetMaxExperience()
+    return gMaxExperience
+end
---
+++ lua/GUIExperience.lua
@@ -0,0 +1,21 @@
+//
+// lua\GUIScoreboard.lua
+//
+// Created by: Alex Hayton (alex.hayton@gmail.com)
+//
+// Manages the experience bar
+//
+
+class 'GUIExperience' (GUIScript)
+
+function GUIExperience:Initialize()
+
+end
+
+function GUIExperience:CreateExperienceBar()
+
+end
+
+function GUIExperience:Update(deltatime)
+
+end
