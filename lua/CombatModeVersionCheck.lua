72733905b70c1b43fb9a28c0faa80341d17d01d7
Alex Hayton <alex.hayton@gmail.com>
2011-03-30 19:54:55 +0100

    Combat mode support for v170

--- lua/CombatModeVersionCheck.lua
+++ lua/CombatModeVersionCheck.lua
@@ -1,5 +1,5 @@
 // This module used to check the version of Combat Mode vs. NS2.
-kCombatModeVersion = 169
+kCombatModeVersion = 170
 
 function CombatMode_VersionCheck()
 	if (Shared.GetBuildNumber() ~= kCombatModeVersion) then
